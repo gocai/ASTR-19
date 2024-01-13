379127a751530074e77812a335b0b6b66670fc71
gocai <123609975+gocai@users.noreply.github.com>
2024-01-12 19:39:20 -0800

    session 2 complete

---
+++ Code Journal #1/Session2.py
@@ -0,0 +1,15 @@
+def PrintSum():
+    x = 1.0 + 1.0
+    print(type(x))
+
+def PrintProduct():
+    x = 2.0 * 2
+    print(type(x))
+
+def main():
+    PrintSum()
+    PrintProduct()
+
+
+if __name__=="__main__": 
+    main() 
